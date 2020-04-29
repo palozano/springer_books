e6139abb190c907a0f7c29e30b2bae5d641ab593
Pablo Lozano <pablo.lozano@uc3m.es>
2020-04-30 00:56:29 +0200

    Final version

--- get_books_EPUB.sh
+++ get_books_EPUB.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 BOOKFILE="book_links_EPUB.txt"
+BOOKFOLDER="EPUBs"
+
+mkdir "$BOOKFOLDER"
 
 idx=1
 while read book; do
         echo $book
-        curl -o $idx $book
+        curl -o $BOOKFOLDER/$idx $book
         idx=$(( $idx +1 ))
 done <$BOOKFILE
--- get_books_PDF.sh
+++ get_books_PDF.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 BOOKFILE="book_links_PDF.txt"
+BOOKFOLDER="PDFs"
+
+mkdir "$BOOKFOLDER"
 
 idx=1
 while read book; do
         echo $book
-        curl -o $idx $book
+        curl -o $BOOKFOLDER/$idx $book
         idx=$(( $idx +1 ))
 done <$BOOKFILE
